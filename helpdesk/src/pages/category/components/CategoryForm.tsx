57c048ab4a5b0f7c800167707077a8918168f711
Tanvi Patel <tanvi.patel@1rivet.com>
2024-10-11 13:14:09 +0530

    feat: set up category module folder structure and routing

---
+++ helpdesk/src/pages/category/components/CategoryForm.tsx
@@ -0,0 +1,11 @@
+
+function CategoryForm() {
+    return (
+        // Add Category
+        <form>
+            <input type="text" name="" id="category" placeholder="Add Categoty" />
+            <button type="button">Submit</button>
+        </form>
+    )
+}
+export default CategoryForm;
---
+++ helpdesk/src/pages/category/hooks/useCategory.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const useCategory = () => {
+    return <h1>use category hook</h1>
+}
+export default useCategory;
