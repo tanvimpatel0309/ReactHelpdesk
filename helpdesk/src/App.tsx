3a284da6f3120c6e96900f8eab3db6478eb5568f
Tanvi Patel <tanvi.patel@1rivet.com>
2024-10-11 12:27:02 +0530

    chore: Added landmark structure with header, sidebar and main section, and applay css for layout and design

--- helpdesk/src/App.tsx
+++ helpdesk/src/App.tsx
@@ -1,11 +1,28 @@
-
 import './App.css'
+import Header from './core/components/Header';
+import Sidebar from './core/components/Sidebar';
 
 function App() {
-
   return (
-   <h1>Helpdesk</h1>
+    // Starts: Landmark structure of Helpdesk app
+    <div className='h-100 d-flex flex-column'>
+      {/* Starts:Header component */}
+      <Header />
+      <div className='d-flex h-100 overflow-hidden'>
+        {/* Starts: Sidebar component */}
+        <Sidebar />
+        {/* Starts: Main content area of the app where the currently selected component is rendered */}
+        <main className='d-flex h-100 w-100 py-4 ps-4 overflow-hidden'>
+          <div className='overflow-auto pe-4'>
+            {/* Page heading */}
+            <h1>Category</h1>
+            {/* Page content */}
+            <p>Rander Category component</p>
+          </div>
+        </main>
+      </div>
+    </div>
+    // Ends: Landmark structure of Helpdesk app
   )
 }
-
 export default App
